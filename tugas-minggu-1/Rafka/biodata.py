0efacd57a8cae41420a93fc7811ebde21ffe5ade
mraclerapz7 <rafka7335@gmail.com>
2026-08-06 19:45:01 +0700

    Submit Minggu 1 - Rafka

---
+++ tugas-minggu-1/Rafka/biodata.py
@@ -0,0 +1,30 @@
+#print("Setup Python Berhasil")
+
+'''
+nama = input("Masukan nama kamu: ")
+jurusan = input("Masukan jurusan kamu: ")
+print ("Selamat datang di Ekstrakurikuler Python, " + nama + " dari jurusan " + jurusan + "!")
+print (f"Selamat datang di Ekstrakurikuler Python, {nama} dari jurusan {jurusan}!")
+'''
+
+print("========================")
+print("FORM PENDAFTARAN BIODATA SISWA")
+print("========================")
+
+nama = input("Nama Lengkap : ")
+jurusan = input("Jurusan : ")
+kelas = input ("Kelas : ")
+hobi = input ("Hobi Utama :")
+cita = input ("Cita-cita :")
+tahun = int(input ("Tahun Lahir :"))
+
+usia = 2026 - tahun 
+print("========================")
+print("KARTU PROFIL DIGITAL SISWA")
+print("========================")
+
+print (f"Nama : {nama}")
+print (f"Jurusan : {jurusan}")
+print (f"Usia : {usia}")
+print (f"Cita Cita : {cita}")
+
