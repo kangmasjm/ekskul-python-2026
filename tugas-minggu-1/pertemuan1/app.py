6b7cd7fa61ea97a13ac0180f24cb0c3fa3fc4a30
hazelanthony29-boop <hazelanthony29@gmail.com>
2026-08-13 13:39:40 +0700

    Add files via upload

---
+++ tugas-minggu-1/pertemuan1/app.py
@@ -0,0 +1,12 @@
+#print("Setup Python Berhasil!")
+
+#A. Cetak Teks (print)
+#Fungsi Print()
+
+nama = input("Masukkan Nama Kamu: ")
+jurusan = input("Masukkan Jurusan Kamu: ")
+print("Selamat datang di Ekstrakurikuler Python, " + nama + " dari jurusan " + jurusan + "!")
+
+#C. F-String (Formatted String Literals)
+#Cara modern, rapi, dan standart industri untuk menggabungkan variabel ke dadlam teks.
+print(f"Selamat datang di Ekstrakurikuler Python, {nama}, dari jurusan {jurusan} !")
