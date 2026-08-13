ed794c90d6334ecb819c91f9fbeb4769003dde1b
ridohub <ridhocakep399@gmail.com>
2026-08-13 17:29:25 +0700

    Add employee salary calculation script

---
+++ tugas-minggu2/ridho/gaji_karyawan.py
@@ -0,0 +1,19 @@
+print("=" * 40)
+print("SISTEM PENGHITUNG GAJI KARYAWAN")
+print("=" * 40)
+
+Nama = input(f"Masukan Nama Karyawan: ")
+Golongan = input(f"Pilih Golongan (A/B/C): ".upper())
+Jam_kerja = input(f"Total Jam Kerja/Minggu: ")
+Rating_performa = input(f"Rating Performa (1 - 5): ")
+
+print("=" * 40)
+print("SLIP GAJI KARYAWAN")
+print("=" * 40)
+
+print(f"Nama Karyawan : {Nama}")
+print(f"Golongan : {Golongan}")
+print(f"Total Jam Kerja/Minggu : {Jam_kerja}")
+print(f"Rating Performa : {Rating_performa}")
+
+print("-" * 40)
