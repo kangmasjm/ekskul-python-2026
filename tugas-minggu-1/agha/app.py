24059e629ecb8d2b27504c0ed417876964f68610
aghajabilahmad-tech <agha.jabilahmad@gmail.com>
2026-08-13 16:32:01 +0700

    Add files via upload

---
+++ tugas-minggu-1/agha/app.py
@@ -0,0 +1,27 @@
+
+print("==============================================")
+print("FORM PENDAFTARAN BIOADATA DIRI")
+print("==============================================")
+
+nama = input("Masukkan Nama Anda        :")
+jurusan = input("Masukkan Jurusan Anda     :")
+hobi = input("Masukkan Hobi Anda        :")
+cita = input("Masukkan Cita-Cita Anda   :")
+lahir = input("Masukkan Tahun Lahir Anda :")
+lahirangka = int(lahir)
+umur = 2026 - lahirangka
+
+print("==============================================")
+print("KARTU PROFIL DIGITAL SISWA")
+print("==============================================")
+
+print(f"Nama      : {nama} ")
+print(f"Jurusan   : {jurusan}")
+print(f"Hobi      : {hobi}")
+print(f"Cita-Cita : {cita}")
+print(f"Lahir     : {lahir}")
+print(f"Umur      : {umur}")
+
+print("---------------------------------")
+print("Pesan Motivasi : Semangat Ngulik untuk masa depan")
+print("==============================================")
