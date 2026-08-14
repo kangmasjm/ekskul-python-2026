5bcfe85f9fe92be49b827aeba120da49cfc9b8c0
chikal7yy <chikalnurjaman@gmail.com>
2026-08-14 08:37:35 +0700

    minggu ke 2
    Added a salary calculation system for employees, including input for name, group selection, total working hours, and performance rating. The program calculates basic salary and overtime based on the selected group.

--- tugas-minggu2/Chikal/gaji.py
+++ tugas-minggu2/Chikal/gaji.py
@@ -1,1 +1,45 @@
+golongan_a = 50000
+golongan_b = 35000
+golongan_c = 25000
 
+print("=" * 60)
+print("             SISTEM PENGHITUNG GAJI KARYAWAN SMK")
+print("=" * 60)
+
+nama = input("Masukkan Nama Karyawan  : ")
+pilih_gol = input("Pilih Golongan (A/B/C)  : ")
+total_jam = int(input("Total Jam Kerja/Minggu  : "))
+rating = int(input("Rating Performa (1 - 5) : "))
+
+
+if pilih_gol.lower() == 'a':
+    tarif = golongan_a
+elif pilih_gol.lower() == 'b':
+    tarif = golongan_b
+elif pilih_gol.lower() == 'c':
+    tarif = golongan_c
+else:
+    tarif = 0
+
+jam_lembur = total_jam - 40
+
+if jam_lembur < 0 :
+    jam_lembur = 0
+
+gaji_pokok = total_jam * tarif
+
+
+
+print("=" * 60)
+print("         SLIP GAJI KARYAWAN")
+print("=" * 60)
+print(f"Nama Karyawan   : {nama}")
+print(f"Golongan    : {pilih_gol.upper()}")
+print(f"Tarif per Jam   : Rp {tarif}")
+print("-" * 60)
+print(f"Gaji Pokok ({total_jam} jam) : Rp {gaji_pokok}")
+print(f"Gaji Lembur ({jam_lembur} jam)  : Rp")
+print(f"Bonus Performa  : Rp")
+print("-" * 60)
+print("TOTAL GAJI DITERIMA: Rp")
+print("=" * 60)
