64eb7214c8571087e0ead852c0a9e109e5a854a3
chikal7yy <chikalnurjaman@gmail.com>
2026-08-14 08:34:52 +0700

    Implement biodata registration form for students
    Added a biodata registration form for students with input prompts and formatted output.

--- tugas-minggu-1/Chikal/bio_generator.py
+++ tugas-minggu-1/Chikal/bio_generator.py
@@ -1,1 +1,22 @@
+print ("==============================")
+print ("FORM PENDAFTARAN BIODATA SISWA")
+print ("==============================")
 
+
+nama= input("Masukan nama :")
+jurusan=input("Masukan Jurusan :")
+kelas=input("X/XI/XII : ")
+hobi=input("Masukan Hobi :")
+citacita=input("Masukan Cita-Cita :")
+tahunlahir= int(input("masukan Tahun Lahir"))
+
+
+print ("==========================")
+print ("KARTU PROFIL DIGITAL SISWA")
+print ("==========================")
+
+print (f"Nama : {nama}")
+print (f"Jurusan : {jurusan}")
+print (f"kelas : {kelas}")
+print (f"Hobi Utama : {hobi}")
+print (f"cita-cita : {citacita}")
