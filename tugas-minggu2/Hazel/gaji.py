39f131bfe2e6c43cc4c6940009fa80f79d4ac779
hazelanthony29-boop <hazelanthony29@gmail.com>
2026-08-20 16:27:09 +0700

    Update gaji.py

--- tugas-minggu2/Hazel/gaji.py
+++ tugas-minggu2/Hazel/gaji.py
@@ -1,35 +1,31 @@
-print("=" * 30)
-print("Sistem Penghitung Gaji Karyawan SMK")
-print("=" * 30)
+print("=== Sistem Penghitung Gaji Karyawan SMK ===")
+nama = input("masukkan Nama Karyawan: ")
+golongan = input("pilih Golongan (A/B/C): ").upper()
+total_jam_kerja = int(input("total Jam Kerja: "))
+rating_performa = float(input("rating Performa (1 - 5): "))
 
+A = int(50000)
+B = int(35000)
+C = int(25000)
 
-nama = input("Masukkan Nama Karyawan : ")
-golongan = input("Pilih Golongan (A/B/C) : ")
-jam_kerja = input("Total Jam Kerja/Minggu : ")
-rating = input("Rating Performa (1 - 5) : ") 
+if golongan == "A":
+    tarif_per_jam = A * total_jam_kerja
+elif golongan == "B":
+    tarif_per_jam = B * total_jam_kerja
+elif golongan == "C":
+    tarif_per_jam = C * total_jam_kerja
 
-if gol == "A":
-    tarif = 50.000
-elif gol == "B":
-    tarif == 35.000
-elif gol == "C":
-    tarif == 20.000
+if total_jam_kerja >= 40 and rating_performa >= 4:
+    jam_lembur = total_jam_kerja - 40
+    gaji_lembur = jam_lembur * tarif_per_jam * 1.5
+    bonus_performa = tarif_per_jam * 0.1
 
-total_kerja = jam_kerja * 1.5
+print("\n=== slip gaji karyawan ===")
+print(f"nama Karyawan: {nama}")
+print(f"golongan: {golongan}")
+print(f"tarif per jam: {tarif_per_jam}")
+print(f"gaji pokok (40 jam kerja): {tarif_per_jam * 40}")
+print(f"gaji lembur ({jam_lembur} jam): {gaji_lembur}")
+print(f"bonus performa: {bonus_performa}")
 
-
-if (total_kerja >= 40):
-    print()
-
-print("=" * 30)
-print("SLIP GAJI KARYAWAN")
-print("=" * 30)
-
-print(f"Nama Karyawan        : {nama}")
-print(f"Golongan             : {golongan}")
-print(f"Tarif Per Jam        : {jam_kerja}")
-
-print("-" * 30)
-
-print(f"Gaji Pokok (40 Jam)        : {total_kerja}")
-print(f"Cita - Cita                : {cita_cita}")
+print(f"Total Gaji: {tarif_per_jam * 40 + gaji_lembur + bonus_performa}")
