3050da7aa16c91b3fdcb03c280e9668feac07f7b
Rakha-html <rakhado354@gmail.com>
2026-08-20 15:56:17 +0700

    Add files via upload

--- tugas-minggu-1/Nadirat/bio_generator.py
+++ tugas-minggu-1/Nadirat/bio_generator.py
@@ -1,1 +1,20 @@
+# variabel
+nama = input("Nama lengkap  : ")
+jurusan = input("jurusan anda  : ")
+Kelas = input("rombel anda   : ")
+hobi = input("Hobi utama anda  : ")
+cita = input("Cita-cita anda   : ")
+tgllahir = int(input("Tahun lahir anda  : "))
+umur =2026 - tgllahir
 
+
+print(f"=====================")
+print(f"  FORM PENDAFTARAN")
+print("=====================")
+print(f"Nama lengkap     : {nama}")
+print(f"Jurusan anda     : {jurusan}")
+print(f"Rombel anda      : {Kelas}")
+print(f"Hobi utama anda  : {hobi}")
+print(f"Cita-cita anda   : {cita}")
+print(f"Tanggal lahir anda : {tgllahir}")
+print(f"Umur anda        : {umur}")
