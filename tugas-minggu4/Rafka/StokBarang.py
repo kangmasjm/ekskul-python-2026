ad3e572939b65077f59dadf673ab933421851aea
Rafka <rafkariandy@smkwikrama.sch.id>
2026-08-28 08:30:24 +0700

    stokbarang,py

---
+++ tugas-minggu4/Rafka/StokBarang.py
@@ -0,0 +1,83 @@
+inventaris = [
+    {"kode": "B01", "nama": "Mouse Wireless", "stok": 15, "harga": 120000},
+    {"kode": "B02", "nama": "Keyboard Mechanical", "stok": 8, "harga": 450000}
+]
+
+print("=" * 55)
+print("SISTEM MENAJEMEN STOK BARANG TOKO SMK")
+print("=" * 55)
+print("[1] Lihat Stok Barang")
+print("[2] Tambah Barang Baru")
+print("[3] Update Stok Barang")
+print("[4] Hapus Barang Dari Inventaris")
+print("=" * 55)
+
+pilihan = input("Pilih Menu (1-4): ")
+
+# --- MENU 1 : READ ---
+if pilihan == "1":
+    print("\n--- DAFTAR INVENTARIS BARANG ---")
+    print(f"{'KODE': <6} | {'NAMA BARANG': <20} | {'STOK': <5} | {'HARGA': <12}")
+    print("-" * 52)
+
+  #MEMBACA LIST SATU PER SATU
+    item1 = inventaris[0]
+    item2 = inventaris[1]
+
+    print(f"{item1['kode']: <6} | {item1['nama']: <20} | {item1['stok']: <5} | {item1['harga']:,}")
+    print(f"{item2['kode']: <6} | {item2['nama']: <20} | {item2['stok']: <5} | {item2['harga']:,}")
+
+#--- MENU 2 : CREATE ---
+elif pilihan == "2":
+    print("\n--- TAMBAH BARANG BARU ---")
+    kode = input("Masukan Kode Barang : ").upper()
+    nama = input("Masukan Nama Barang: ")
+    stok = int(input("Masukan Jumlah Stok : "))
+    harga = int(input("Masukan Harga Barang : "))
+
+     #MEMBUAT DICTIONARY BARU
+    barang_baru = {"kode": kode, "nama": nama, "stok": stok, "harga": harga}
+
+    #TAMBAH LIST KE INVENTARIS
+    inventaris.append(barang_baru)
+    print(f"\n Berhasil! Barang '{nama}' dengan {kode} ditambahkan")
+
+    item1 = inventaris[0]
+    item2 = inventaris[1]
+    item3 = inventaris[2]
+        
+    print(f"{item1['kode']: <6} | {item1['nama']: <20} | {item1['stok']: <5} | {item1['harga']:,}")
+    print(f"{item2['kode']: <6} | {item2['nama']: <20} | {item2['stok']: <5} | {item2['harga']:,}")
+    print(f"{item3['kode']: <6} | {item3['nama']: <20} | {item3['stok']: <5} | {item3['harga']:,}")
+
+
+#--- MENU 3 : UPDATE ---
+elif pilihan == "3":
+    print("\n--- UPDATE STOK BARANG ---")
+    print(f"1. {inventaris[0]['nama']} (Stok saat ini : {inventaris[0]['stok']})")
+    print(f"1. {inventaris[1]['nama']} (Stok saat ini : {inventaris[1]['stok']})")
+
+    idx = int(input("Pilihan nomor barang yang ingin di hapus (1/2) : ")) - 1
+    stok_baru = int(input(f"masukan jumlah stok terbaru : "))
+
+  
+    #UPDATE STOK
+    inventaris[idx]["stok"] = stok_baru
+    print(f"\n Stok '{inventaris[idx]['nama']}' berhasil diperbarui menjadi {stok_baru} unit.")
+
+
+#--- MENU 4 : DELETE ---
+elif pilihan == "4":
+    print("\n --- HAPUS BARANG --- ")
+    print(f"1. {inventaris[0]['nama']}")
+    print(f"2. {inventaris[1]['nama']}")
+
+    idx = int(input("Pilih no barang yang ingin dihapus")) - 1
+    barang_dihapus = inventaris.pop(idx)
+
+    print(f"\n barang '{barang_dihapus}' berhasil di hapus dari sistem")
+
+else:
+    print("\n pilihan menu tidak valid")
+
+    print("=" * 55)
